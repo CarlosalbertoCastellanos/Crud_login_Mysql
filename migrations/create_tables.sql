760b35d37431820074e11c2792d571fb9910a107
JuanPa-CT <juanpablo.castellanos@novastudio.es>
2024-09-04 09:37:46 -0500

    update table

--- migrations/create_tables.sql
+++ migrations/create_tables.sql
@@ -1,17 +1,12 @@
-CREATE TABLE smartphoneUser(
-    id int(11) NOT NULL,
-    username varchar(120) NOT NULL,
-    password varchar(120) NOT NULL,
-    fullName varchar(50)  NOT NULL
+CREATE TABLE IF NOT EXISTS user(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(120) NOT NULL,
+    password VARCHAR(120) NOT NULL,
+    fullName VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-ALTER TABLE smartphoneUser ADD PRIMARY KEY (id);
-
-ALTER TABLE smartphoneUser
-MODIFY id INT(11) NOT NULL AUTO_INCREMENT;
-
-
-CREATE TABLE smartphone(
+CREATE TABLE IF NOT EXISTS  smartphone(
     id INT(11) NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     serie VARCHAR(50) NOT NULL,
@@ -21,5 +16,34 @@ CREATE TABLE smartphone(
     user_id INT(11),
     create_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES smartphoneUser(id)
+    FOREIGN KEY (user_id) REFERENCES user(id)
 );
+
+CREATE TABLE IF NOT EXISTS shoppingcart(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    user_id INT(11) NOT NULL,
+    smartphone_id INT(11) NOT NULL,  
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (smartphone_id) REFERENCES smartphone(id)
+);
+
+CREATE TABLE IF NOT EXISTS orderStore(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    user_id INT(11) NOT NULL,
+    order_status ENUM('OK', 'Cancel'),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+CREATE TABLE IF NOT EXISTS orderSmartphone(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    quantity INT(11) NOT NULL,  
+    user_id INT(11) NOT NULL,
+    smartphone_id INT(11) NOT NULL,  
+    order_id INT(11) NOT NULL,  
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (smartphone_id) REFERENCES smartphone(id)
+    FOREIGN KEY (order_id) REFERENCES orderStore(id)
+);
+
