cd8e96b78009afaaeb72aade5125afc76db0784a
Carlos Castellanos <Ccastellanosgarzon@gmail.com>
2024-09-15 23:04:17 -0500

    New dependencies are added

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,11 +1,17 @@
-
 import express from "express";
+import routerLogin from "./login/login.js";
 import routersBuy from "./orderBuy/index.js";
 import statusCode from "../../utils/statusCode.js";
 
 const routerApi = express.Router();
+
+// Ruta principal para verificar que el servidor funciona
 routerApi.get("/", (req, res) => {
-    res.status(statusCode.ok).send("herllo");
+  res.status(statusCode.ok).send("hello");
 });
-routerApi.use("/orderBuy", routersBuy)
-export default routerApi
+
+// Agregar rutas de la API
+routerApi.use("/login", routerLogin);
+routerApi.use("/orderBuy", routersBuy);
+
+export default routerApi;
