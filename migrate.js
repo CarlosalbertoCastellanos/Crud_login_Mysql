7aa25407f54bd4023ee94e87e24cebcbc83c022b
Valeria Rojas <camirojas@MacBook-Air-de-Camilo.local>
2024-09-16 12:46:07 -0500

    valeria db changes to run app

--- migrate.js
+++ migrate.js
@@ -9,7 +9,8 @@ const __dirname = path.dirname(__filename);
 const config = {
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "example",
+  port: process.env.DB_PORT || 8889,
+  password: process.env.DB_PASSWORD || "root",
   database: process.env.DB_NAME || "smartphone",
 };
 
--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,7 @@
 export const database = {
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
+  port: process.env.DB_PORT || 8889, // YO VALERIA USO ESE PUERTO POR QUE USO MAMP, NO ME SIRVE DOCKER
   password: process.env.DB_PASSWORD || "example",
   database: process.env.DB_NAME || "smartphone",
   waitForConnections: true,
