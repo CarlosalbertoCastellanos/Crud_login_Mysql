3e8450651dbb80daa0d5dc7456409813cba21d9f
Carlos Castellanos <Ccastellanosgarzon@gmail.com>
2024-09-19 21:56:25 -0500

    change of route

--- src/routes/api/user/index.js
+++ src/routes/api/user/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import routerSmartphone from "./smartPhone/index.js";
-import routerOrderBuy from "./orderBuy/index.js";
+import routerOrderBuy from "./OrderBuy/index.js";
 
 const routerUser = express.Router();
-routerUser.use('/:userId/orderBuy', routerOrderBuy);
+routerUser.use("/:userId/orderBuy", routerOrderBuy);
 routerUser.use("/:userId/smartphone", routerSmartphone);
-export default routerUser
+export default routerUser;
