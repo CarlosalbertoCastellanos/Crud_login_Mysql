0fbecba8b458abaa02650d19045512afefb789c8
Carlos Castellanos <Ccastellanosgarzon@gmail.com>
2024-09-23 19:46:30 -0500

    remove duplicate line code

--- src/routes/api/user/index.js
+++ src/routes/api/user/index.js
@@ -5,9 +5,7 @@ import routerNewUser from "../NewUsers/index.js";
 import routerOrderBuy from "./OrderBuy/index.js";
 
 const routerUser = express.Router();
-routerUser.use('/:userId/orderBuy', routerOrderBuy);
-routerUser.use('/:userId/smartphone', routerSmartphone);
-routerUser.use('/:userId/logout', routerNewUser);
-export default routerUser 
-import routerOrderBuy from "./OrderBuy/index.js";
-
+routerUser.use("/:userId/orderBuy", routerOrderBuy);
+routerUser.use("/:userId/smartphone", routerSmartphone);
+routerUser.use("/:userId/logout", routerNewUser);
+export default routerUser;
